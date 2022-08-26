8d362a68c279996b58f487aeffed1772c5110451
Gary <chankarlit2727@gmail.com>
2022-08-26 15:40:59 +0800

    Display data to datatable with server side, implement delete button to remove row data

--- transfer/settings.py
+++ transfer/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'transfer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'statics'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
